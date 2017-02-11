3d5459aa35124a304fbe15cbbe7ed39506bb3029
ezat123 <ezat.bscs@gmail.com>
2017-02-11 17:13:48 +0500

    addOnlineBookStore

--- index2.js
+++ index2.js
@@ -2,31 +2,26 @@ import React from 'react';
 import ReactDOM from "react-dom"
 import {Router,Route,Link,hashHistory,IndexRoute} from 'react-router'
 import './index.css'
-import './color.css'
-import './style/pd.css'
-import './style/border.css'
-const Modal = require('./modal.jsx')
-const Cart = require('./cart.jsx')
-const Checkout = require('./checkout.jsx')
-const Product = require('./product.jsx')
+import Cart from './cart.js'
+import Product from './product'
+import Checkout from './checkout'
+import Modal from './modal.jsx'
+
 const PRODUCTS = [
-  { id: 0, src: 'https://i.imgur.com/mVhn1xd.jpg', title: 'Pro Express.js', url: 'http://amzn.to/1D6qiqk' },
-  { id: 1, src: 'http://whatpixel.com/images/2016/09/react-up-and-running.jpg', title: 'Practical Node.js', url: 'http://amzn.to/NuQ0fM' },
-  { id: 2, src: 'http://blog.krawaller.se/img/reactedge.jpg', title: 'Express API Reference', url: 'http://amzn.to/1xcHanf' },
-  { id: 3, src: 'http://whatpixel.com/images/2016/09/reactjs-essentials-book-cover.jpg', title: 'React Quickly', url: 'https://www.manning.com/books/react-quickly'},
-  { id: 4, src: 'https://www.packtpub.com/sites/default/files/B04784_MockupCover_Cookbook.jpg', title: 'Full Stack JavaScript', url: 'http://www.apress.com/9781484217504'}
+  { id: 0, src: 'images/proexpress-cover.jpg', title: 'Pro Express.js', url: 'http://amzn.to/1D6qiqk' },
+  { id: 1, src: 'images/practicalnode-cover.jpeg', title: 'Practical Node.js', url: 'http://amzn.to/NuQ0fM' },
+  { id: 2, src: 'images/expressapiref-cover.jpg', title: 'Express API Reference', url: 'http://amzn.to/1xcHanf' },
+  { id: 3, src: 'images/reactquickly-cover.jpg', title: 'React Quickly', url: 'https://www.manning.com/books/react-quickly'},
+  { id: 4, src: 'images/fullstack-cover.png', title: 'Full Stack JavaScript', url: 'http://www.apress.com/9781484217504'}
 ]
-
-function Heading(){
-  return (
-    <h1 className='black t-white pd-15 bdr-r-5'>Buy a Book from Bilal online Book Store</h1>
-  )
+const Heading = () => {
+  return <h1>Bilal Book Store</h1>
 }
-var Copy = ()=> <p>Please click on a book to view details in a modal. You can copy/paste the link of the modal. The link will open book on a separate page.</p>      
-
+const Copy = () => {
+  return <p>Please click on a book to view details in a modal. You can copy/paste the link of the modal. The link will open book on a separate page.</p>
+}
 class App extends React.Component {
   componentWillReceiveProps(nextProps) {
-    console.log(nextProps)
     this.isModal = (nextProps.location.state &&
       nextProps.location.state.modal)
     if (this.isModal &&
@@ -56,9 +51,9 @@ class App extends React.Component {
 class Index extends React.Component {
   render() {
     return (
-      <div className="skyblue bdr-r-5 pd-15">
+      <div>
         <Copy/>
-        <p><Link to="/cart" className="btn black t-white">Cart</Link></p>
+        <p><Link to="/cart" className="btn btn-danger">Cart</Link></p>
         <div>
           {PRODUCTS.map(picture => (
             <Link key={picture.id}
@@ -82,7 +77,6 @@ const addToCart = (id) => {
   else
     cartItems[id] = 1
 }
-
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
--- todo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'
-var root = document.getElementById('root')
-
-ReactDOM.render(
-    <h1>helo karac</h1>,
-    root
-)
